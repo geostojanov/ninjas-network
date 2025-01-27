b59f96e6142909f34f2b7fa26acf0600874d4264
geostojanov <gestojanov@gmail.com>
2025-01-27 17:40:09 +0100

    Adding CSS & Tailwind

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,18 +1,13 @@
 <x-layout>
   <h2>Currently Available Ninjas</h2>
 
-  @if ($greeting == 'hello')
-    <p>Hi from inside if statement</p>
-  @endif
-
-  <p>{{ $greeting }}</p>
   <ul>
-    @foreach ($ninjas as $ninja)
+    @foreach($ninjas as $ninja)
       <li>
-        <x-card href="/ninjas/{{ $ninja['id'] }}" :highlight="$ninja['skill'] > 70">
+        <x-card :highlight="$ninja['skill'] > 70" href="/ninjas/{{$ninja['id']}}">
           <h3>{{ $ninja['name'] }}</h3>
         </x-card>
       </li>
-    @endforeach    
+    @endforeach
   </ul>
 </x-layout>
